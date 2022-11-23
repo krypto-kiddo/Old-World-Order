5e1c214296fee25cc8c1edc41fe534c7cff760fe
Krypto Kiddo <97212160+krypto-kiddo@users.noreply.github.com>
2022-11-23 22:20:36 +0530

    Update OldWorldOrderContract.sol

--- OldWorldOrderContract.sol
+++ OldWorldOrderContract.sol
@@ -1,1 +1,38 @@
-// Heh you thought I'd put it out here ? Not just yet my friend, not just yet ... UwU
+// I finally posted the code here my friend.
+// Things have been a rollercoaster but yea
+// Hate it when people are dumb enough to not find my easter eggs, but i guess that's the point ;-;
+
+// Code written by Krypto-Kiddo
+// OWO token ERC1155 original smartcontract code
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract OldWorldOrder is ERC1155 {
+
+    uint256 public constant OwO_Coin = 0;
+    uint256 public constant OwO_NFT_1 = 1;
+    uint256 public constant OwO_NFT_2 = 2;
+    uint256 public constant OwO_NFT_3 = 3;
+    uint256 public constant OwO_NFT_4 = 4;
+
+    constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmfWmF6mZ4ZjmKw3NeeeHj5s3v76LSd5gv2iUBJBDcj3ZD/{id}.json"){
+        _mint(msg.sender, OwO_Coin, 666666666, "");
+        _mint(msg.sender, OwO_NFT_1, 3, "");
+        _mint(msg.sender, OwO_NFT_2, 3, "");
+        _mint(msg.sender, OwO_NFT_3, 3, "");
+        _mint(msg.sender, OwO_NFT_4, 3, "");
+    }
+
+    function uri(uint256 _tokenid) override public view returns (string memory) {
+        return string(
+            abi.encodePacked(
+                "https://gateway.pinata.cloud/ipfs/QmfWmF6mZ4ZjmKw3NeeeHj5s3v76LSd5gv2iUBJBDcj3ZD/",
+                Strings.toString(_tokenid),".json"
+            )
+        );
+    }
+}
